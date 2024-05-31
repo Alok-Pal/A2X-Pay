6d4e742224fafcc544fbbe506cdb7b805a3fe96c
Sparshlohana <sparsh.lohana@gmail.com>
2024-06-01 00:51:48 +0530

    layout created with side bar and navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import React from "react";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
-const App = () => {
-  return <RouterProvider router={router} />;
-};
+import React, { lazy, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom'
+import initializeApp from "./app/init";
+import checkAuth from "./app/auth";
+import { themeChange } from 'theme-change'
+import Layout from "./containers/Layout";
+
+const Login = lazy(() => import('./pages/Login'))
+
+initializeApp()
+
+// const token = checkAuth()
+
+function App() {
+
+  useEffect(() => {
+    // 👆 daisy UI themes initialization
+    themeChange(false)
+  }, [])
+
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+
+          {/* Place new routes over this */}
+          <Route path="/app/*" element={<Layout />} />
+
+          {/* <Route path="*" element={<Navigate to={token ? "/app/welcome" : "/login"} replace />} /> */}
+
+        </Routes>
+      </Router>
+    </>
+  )
+}
 
 export default App;
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,2 +1,17 @@
-import router from "./BrowserRouter";
-export { router };
+// All components mapping with path for internal routes
+
+import { Squares2X2Icon } from '@heroicons/react/24/outline'
+
+const iconClasses = `h-6 w-6`
+
+
+const routes = [
+
+  {
+    path: '/app/dashboard',
+    icon: <Squares2X2Icon className={iconClasses} />,
+    name: 'Dashboard',
+  },
+
+]
+export default routes
